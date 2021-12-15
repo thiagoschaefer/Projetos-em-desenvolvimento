e915ee835fb674293258154ddf11ac4328f7a332
Thiago Schaefer <thiagoschaefer_bmx@hotmail.com>
2021-12-15 17:18:38 -0300

    Update script.js

---
+++ 1-Calculadora/script/script.js
@@ -0,0 +1,22 @@
+/*var a = window.document.querySelector('button#num')
+
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar() {
+    a.style.background = 'red'
+}
+function entrar() {
+    a.style.background = 'rgba(214, 211, 211, 1.992)'
+}
+function sair() {
+    a.style.background = 'white'
+}*/
+
+function calculo() {
+    var valor1 = window.document.getElementById('display')
+    var v1 = Number(valor1.value)
+}
+ 
+console.log(calculo)
