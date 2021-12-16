fe736804ddcc750ce5722eb77acd19333ba088a2
Thiago Schaefer <thiagoschaefer_bmx@hotmail.com>
2021-12-16 18:24:45 -0300

    teclado funcionando

--- 1-Calculadora/script/script.js
+++ 1-Calculadora/script/script.js
@@ -2,6 +2,7 @@
 window.onload = function() {
     colorbutton();
     bgrandon();
+    //registradisplay();
     //validatecla();
 }
 
@@ -19,24 +20,33 @@ function bgrandon(){
     }
 }
 
-//Altera cor de teclas
+//Altera cor de teclas quando, "clica - passa sobre - sai". incluir condição for
 function colorbutton(){
-var a = window.document.getElementsByTagName('button')[2]
+    var filhos = document.querySelectorAll('#number num')
+    for (var i = 0; i < filhos.length; i++) {
+        filhos[i].addEventListener('click', clicar)
+        filhos[i].addEventListener('mouseenter', entrar)
+        filhos[i].addEventListener('mouseout', sair)
+        
+            function clicar() {
+                filhos.style.background = 'green'
+            }
+            function entrar() {
+                filhos.style.background = 'rgba(214, 211, 211, 1.992)'
+            }
+            function sair() {
+                filhos.style.background = 'white'
+            }
+        }
+    }
 
-a.addEventListener('click', clicar)
-a.addEventListener('mouseenter', entrar)
-a.addEventListener('mouseout', sair)
+    
+  
 
-function clicar() {
-    a.style.background = 'green'
-}
-function entrar() {
-    a.style.background = 'rgba(214, 211, 211, 1.992)'
-}
-function sair() {
-    a.style.background = 'white'
-}
-}
+
+/*let a = window.document.activeElement.value;
+console.log(a)*/
+
 
 // Indentifica e exibe o código de uma tecla que foi apertada no teclado
 function validatecla() {
@@ -47,20 +57,54 @@ window.document.querySelector('body').addEventListener('keydown', function(event
 });
 }
 
-/* Criar uma condição que valide as 16 teclas da claculadora que podem ser acionadas pelo teclado - condição "while" */
-
-
+// Criar uma condição que valide as 16 teclas da claculadora que podem ser acionadas pelo teclado - condição "while" 
 
 // Captura o que for digitado no display da calculadora 
-/*var valorA = window.document.activeElement.value
+function guardavalor(){
+let valorA = window.document.activeElement.value
 console.log(valor)
 
 if (event.keycode = 97) {
     window.document.activeElement.innerText = '3' 
-}*/
+}
+}
 
+//registrar numeros no display
 
-
+function registradisplay(x) {
+    switch (x) {
+        case x = 0: 
+        document.getElementById('display').value = '0'
+        break
+        case a = 1:
+        document.getElementById('display').value = '1'
+        break
+        case a = 2:
+        document.getElementById('display').value = '2'
+        break
+        case a = 3:
+        document.getElementById('display').value = '3'
+        break
+        case a = 4: 
+        document.getElementById('display').value = '4'
+        break
+        case a = 5:
+        document.getElementById('display').value = '5'
+        break
+        case a = 6:
+        document.getElementById('display').value = '6'
+        break
+        case a = 7:
+        document.getElementById('display').value = '7'
+        break
+        case a = 8:
+        document.getElementById('display').value = '8'
+        break
+        case a = 9:
+        document.getElementById('display').value = '9'
+        break
+    }
+}
 
 
 
