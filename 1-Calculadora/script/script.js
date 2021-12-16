8b081a97921d5ea2053e75febb753345317d6242
Thiago Schaefer <thiagoschaefer_bmx@hotmail.com>
2021-12-16 13:57:05 -0300

    Usando window.onload

--- 1-Calculadora/script/script.js
+++ 1-Calculadora/script/script.js
@@ -1,52 +1,34 @@
 
+window.onload = function() {
+    colorbutton();
+    bgrandon();
+    //validatecla();
+}
+
 //Mudar a cor do Background da calculadora
-var btn = document.querySelector('button');
-
-function random(number) {
-  return Math.floor(Math.random()*(number+1));
+function bgrandon(){
+    var btn = document.querySelector('button');
+    
+    function random(number) {
+        return Math.floor(Math.random()*(number+1));
+    }
+    
+    btn.onclick = function() {
+        var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+        document.body.style.backgroundColor = rndCol;
+    }
 }
-
-btn.onclick = function() {
-  var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
-}
-
-
-// Indentifica e exibe o código de uma tecla que foi apertada no teclado
-
-    window.document.querySelector('body').addEventListener('keydown', function(event) {
-
-    window.alert('O código da tecla pressionada é :' + event.keyCode)
- 
- });
-
-/* Criar uma condição que valide as 16 teclas da claculadora que podem ser acionadas pelo teclado - condição "while" */
-
-
-
-/* Captura o que for digitado no display da calculadora */
-var valorA = window.document.activeElement.value
-console.log(valor)
-
-if (event.keycode = 97) {
-    window.document.activeElement.innerText = '3' 
-}
-
-
-
-
-
 
 //Altera cor de teclas
-/*
-var a = window.document.querySelector('button#num')
+function colorbutton(){
+var a = window.document.getElementsByTagName('button')[2]
 
 a.addEventListener('click', clicar)
 a.addEventListener('mouseenter', entrar)
 a.addEventListener('mouseout', sair)
 
 function clicar() {
-    a.style.background = 'red'
+    a.style.background = 'green'
 }
 function entrar() {
     a.style.background = 'rgba(214, 211, 211, 1.992)'
@@ -54,7 +36,34 @@ function entrar() {
 function sair() {
     a.style.background = 'white'
 }
-*/
+}
+
+// Indentifica e exibe o código de uma tecla que foi apertada no teclado
+function validatecla() {
+window.document.querySelector('body').addEventListener('keydown', function(event) {
+    
+    window.alert('O código da tecla pressionada é :' + event.keyCode)
+    
+});
+}
+
+/* Criar uma condição que valide as 16 teclas da claculadora que podem ser acionadas pelo teclado - condição "while" */
+
+
+
+// Captura o que for digitado no display da calculadora 
+/*var valorA = window.document.activeElement.value
+console.log(valor)
+
+if (event.keycode = 97) {
+    window.document.activeElement.innerText = '3' 
+}*/
+
+
+
+
+
+
 
 
 
